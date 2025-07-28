e77cf9f904bd1740518a43b3d679176aeebeb222
Jibril Nikki <jibrilnikki.g@gmail.com>
2025-07-28 13:31:30 +0700

    Update kab_bandung.py

--- Project_BPS/views/kab_bandung.py
+++ Project_BPS/views/kab_bandung.py
@@ -158,7 +158,7 @@ with col1:
             style_function=lambda feature: {
                 'fillColor': 'blue', 'color': 'black', 'weight': 0.9, 'fillOpacity': 0
             },
-            tooltip=folium.features.GeoJsonTooltip(fields=[KABKOT])
+            tooltip=folium.features.GeoJsonTooltip(fields=['KABKOT'])
         ).add_to(m)
 
     if not df_uploaded.empty:
